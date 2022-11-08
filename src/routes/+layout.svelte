5ebcf5a8ef6de491e3bb3bf63aab54c496b358c2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 20:53:10 +0100

    feat: :sparkles: add ugly nav bar at the top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
-export let data;
+  export let data;
+  import { base } from "$app/paths";
 </script>
+
+<nav>
+  {#if data.myMenu}
+    <ul>
+      {#each data.myMenu as link}
+        <li>
+          <a href="{base}/{link.title}">{link.title}</a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</nav>
 
 <slot />
 
