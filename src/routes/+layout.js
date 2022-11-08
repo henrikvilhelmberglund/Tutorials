7eecafaf3e0d116a8b5ae64cb6fc3f06f296d2e1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 17:39:20 +0100

    feat: :sparkles: add load function in +layout.js and remove it from +layout.svelte

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,21 @@
 export const prerender = true;
+
+const modules = import.meta.glob([
+  "/src/lib/md/**.md",
+  "/src/lib/md/learning-Svelte-and-SvelteKit/**.md",
+]);
+let myMenu = [];
+for (let path in modules) {
+  let fixedPath = path.replace(".md", "");
+  myMenu.push({
+    title: fixedPath.substring(fixedPath.lastIndexOf("/") + 1),
+    link: fixedPath,
+  });
+}
+console.log(myMenu);
+
+export async function load() {
+  return {
+    myMenu
+  };
+}
