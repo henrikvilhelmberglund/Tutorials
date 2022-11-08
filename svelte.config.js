f735129a82a8eac9eb650343484119d154f5746c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 14:47:21 +0100

    feat: :sparkles: add static adapter and add base url to it

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
+//put the lines below inside of kit: { } in svelte.config.js
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
+  },
+  paths: {
+    base: dev ? "" : "/Tutorials"
+  },
 };
 
 export default config;
