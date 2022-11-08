1f51d20a9f17f985aea48860051190293c3b3b67
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 20:46:14 +0100

    fix: :bug: fix base path (was outside of kit)

--- svelte.config.js
+++ svelte.config.js
@@ -18,11 +18,11 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: dev ? "" : "/Tutorials",
+    },
   },
 
-  paths: {
-    base: dev ? "" : "/Tutorials",
-  },
 
   preprocess: [mdsvex(mdsvexConfig)],
 };
