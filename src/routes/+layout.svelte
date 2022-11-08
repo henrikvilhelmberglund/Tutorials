801ae60fd16ce5dcf834a1b79729acc916c11976
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 17:33:13 +0100

    feat: :sparkles: add +layout.svelte which will save all .mds in certain folders in an array

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  const modules = import.meta.glob([
+    "/src/lib/md/**.md",
+    "/src/lib/md/learning-Svelte-and-SvelteKit/**.md",
+  ]);
+  let myMenu = [];
+  for (let path in modules) {
+    let fixedPath = path.replace(".md", "");
+    myMenu.push({
+      title: fixedPath.substring(fixedPath.lastIndexOf("/") + 1),
+      link: fixedPath,
+    });
+  }
+  console.log(myMenu);
+</script>
+
+<slot />
+
+<style>
+</style>
