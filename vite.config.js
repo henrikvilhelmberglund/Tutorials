1220cbc9ffb038e745d7a2efc20a96ff2cc8c74f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-08 20:47:02 +0100

    build: :sparkles: add esnext target to support top level await
    (what does this even mean)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import examples from 'mdsvexamples/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit(), examples]
+  plugins: [sveltekit(), examples],
+  build: {
+    target: 'esnext'
+  },
 };
 
 export default config;
