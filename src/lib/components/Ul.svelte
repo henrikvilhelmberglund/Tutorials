6beeba6d09a564061e345b53bb33ebaedbc19f46
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-24 13:11:44 +0100

    fix: :fire: remove random emmet testing thingy (oops)   it was ul>li* btw when selecting several lines

--- src/lib/components/Ul.svelte
+++ src/lib/components/Ul.svelte
@@ -1,9 +1,4 @@
-<ul class="bg-slate-300 rounded-2xl self-start ml-6 shadow-xl">
+<ul class="bg-slate-300 rounded-3xl self-start ml-6 shadow-xl">
   <slot />
 </ul>
 
-<ul>
-  <li>line</li>
-  <li>anotherline</li>
-  <li>morelines</li>
-</ul>
