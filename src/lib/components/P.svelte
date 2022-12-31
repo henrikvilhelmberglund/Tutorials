e3ce666828b7316b3e035e013db86f14f6f5db7d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-31 16:40:52 +0100

    feat: :sparkles: add way to override classes from outside in P component

--- src/lib/components/P.svelte
+++ src/lib/components/P.svelte
@@ -1,3 +1,8 @@
-<p class="text-1xl font-medium font-sans text-blue-600 self-start pl-4">
+<script>
+  export let _class = "";
+</script>
+
+<p
+  class="text-1xl font-medium font-sans text-blue-600 self-start pl-4 {_class}">
   <slot />
 </p>
