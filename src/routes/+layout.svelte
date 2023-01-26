1f8427dca599586e319b74afd873be6a93469bd6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 13:51:09 +0100

    fix: :bug: use tabs again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,29 @@
 <script>
-  export let data;
-  import { base } from "$app/paths";
+	export let data;
+	import { base } from "$app/paths";
 </script>
 
 <nav>
-  {#if data.myPages}
-    <ul>
-      {#each data.myPages as link}
-        <li>
-          <a href="{base}/{link.title}">{link.title}</a>
-        </li>
-      {/each}
-    </ul>
-  {/if}
+	{#if data.myPages}
+		<ul>
+			{#each data.myPages as link}
+				<li>
+					<a href="{base}/{link.title}">{link.title}</a>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </nav>
 
 <div class="w-full bg-red-900 [&>*]:p-2">
-  <p class="text-red-300 text-6xl">Warning!</p>
-  <p class="text-red-100 text-3xl">under construction :)</p>
+	<p class="text-red-300 text-6xl">Warning!</p>
+	<p class="text-red-100 text-3xl">under construction :)</p>
 </div>
 
 <slot />
 
 <style uno:preflights uno:safelist global>
   :global(body) {
-    @apply bg-slate-300 m-0;
-  }
+		@apply m-0 bg-slate-300;
+	}
 </style>
