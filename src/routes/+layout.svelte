f0b8305716eb25b4ab0d56d2abe6f62d02dee71c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 15:22:01 +0100

    fix: :fire: remove nav from root layout because it will be visible everywhere otherwise

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,6 @@
 <script>
 	export let data;
-	import { base } from "$app/paths";
 </script>
-
-<nav>
-	{#if data.myPages}
-		<ul>
-			{#each data.myPages as link}
-				<li>
-          {#if !link.bsvelte}
-					<a href="{base}/{link.title}">{link.title}</a>
-          {:else}
-					<a href="{base}/svelte-course/{link.title}">{link.title}</a>
-          {/if}
-				</li>
-			{/each}
-		</ul>
-	{/if}
-</nav>
 
 <div class="w-full bg-red-900 [&>*]:p-2">
 	<p class="text-red-300 text-6xl">Warning!</p>
