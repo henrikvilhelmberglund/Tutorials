3262391578c249423abf79b102aaeb735e51dbad
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 15:23:56 +0100

    feat: :sparkles: add nav for svelte-course using data.myPages.bsvelte

---
+++ src/routes/svelte-course/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let data;
+	import { base } from "$app/paths";
+</script>
+
+<nav>
+	{#if data.myPages}
+		<ul>
+			{#each data.myPages as link}
+      {#if link.bsvelte}
+				<li>
+					<!-- <a href="{base}/{link.title}">{link.title}</a> -->
+          <!-- {:else} -->
+					<a href="{base}/svelte-course/{link.title}">{link.title}</a>
+				</li>
+        {/if}
+			{/each}
+		</ul>
+	{/if}
+</nav>
+
+<slot />
