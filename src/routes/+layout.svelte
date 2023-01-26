dd3c7d05f3a858426c88bdf7b2c465f206c9c5a9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 13:26:58 +0100

    fix: :bug: move warning to +layout.svelte to make it less annoying, remove margin from body

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,10 +15,15 @@
   {/if}
 </nav>
 
+<div class="w-full bg-red-900 [&>*]:p-2">
+  <p class="text-red-300 text-6xl">Warning!</p>
+  <p class="text-red-100 text-3xl">under construction :)</p>
+</div>
+
 <slot />
 
 <style uno:preflights uno:safelist global>
   :global(body) {
-    @apply bg-slate-300;
+    @apply bg-slate-300 m-0;
   }
 </style>
