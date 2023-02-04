5cf21ef34ab58c567a6ade2c3137f9fbab6aace7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-04 16:27:29 +0100

    feat: :sparkles: add ShowCode component that uses prism to highlight code coming from ?raw imports

---
+++ src/lib/ShowCode.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name;
+	export let code;
+	import Prism from "prismjs";
+	import "prism-svelte";
+	const highlighted = Prism.highlight(code, Prism.languages.svelte, "svelte");
+</script>
+
+<!-- {highlighted} -->
+
+<div class="relative">
+	<pre class="language-svelte rounded-xl">
+<p
+			class="rounded-r-0 rounded-bl-0 absolute top-0 left-2 rounded-xl bg-orange-400 p-1 text-black">{name}</p>
+<code
+			style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+monospace;">{@html highlighted}</code>
+  </pre>
+</div>
