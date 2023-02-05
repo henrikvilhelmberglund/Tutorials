b6961200b07efb446a4350de38aac83e80bf86a9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-05 16:11:55 +0100

    feat: :sparkles: add svelte-course 012

---
+++ src/lib/md/svelte-course/012 - bind group/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let flavors = [];
+</script>
+
+<label>
+	<input type="checkbox" bind:group={flavors} value="1" />1
+</label>
+<label>
+	<input type="checkbox" bind:group={flavors} value="2" />2
+</label>
+<label>
+	<input type="checkbox" bind:group={flavors} value="3" />3
+</label>
+
+Flavors: {flavors.join(",") || "-"}
+
+<style>
+</style>
