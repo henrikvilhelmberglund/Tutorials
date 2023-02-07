17e83a7b70407e07ee03b0dcf6a540784093c6e9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-07 23:40:57 +0100

    feat: :sparkles: add scuffed minimize button for ShowCode

--- src/lib/ShowCode.svelte
+++ src/lib/ShowCode.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let name;
 	export let code;
+	let show = true;
 	import Prism from "prismjs";
 	import "prism-svelte";
 	const highlighted = Prism.highlight(code, Prism.languages.svelte, "svelte");
@@ -17,17 +18,26 @@
 
 	<div class="relative">
 		{#if $$slots.default}
-			<pre class="language-svelte rounded-t-0 !m-0 rounded-xl p-0 !pt-10"><p
-					class="rounded-r-0 rounded-bl-0 rounded-t-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
-					style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
-      monospace;">{@html highlighted}</code>
-    </pre>
+			<button on:click={() => (show = !show)} class="absolute top-0 right-0"
+				>{show ? "🔼" : "🔽"}</button>
+        <p class="rounded-r-0 rounded-bl-0 rounded-t-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p>
+			{#if show}
+				<pre class="language-svelte rounded-t-0 !m-0 rounded-xl p-0 !pt-10"><code
+						style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+          monospace;">{@html highlighted}</code>
+        </pre>
+			{/if}
 		{:else}
-			<pre class="language-svelte !m-0 rounded-xl p-0 !pt-10"><p
-					class="rounded-r-0 rounded-bl-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
-					style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
-        monospace;">{@html highlighted}</code>
-      </pre>
+			<button on:click={() => (show = !show)} class="absolute top-0 right-0"
+				>{show ? "🔼" : "🔽"}</button>
+			{#if show}
+      <p class="rounded-r-0 rounded-bl-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p>
+				<pre class="language-svelte !m-0 rounded-xl p-0 !pt-10">
+					<code
+						style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+          monospace;">{@html highlighted}</code>
+        </pre>
+			{/if}
 		{/if}
 	</div>
 </div>
