7e2b66a98bc6e07659c96c4f8619fede7fdd863e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-07 15:43:51 +0100

    feat: :sparkles: add slots functionality to ShowCode

--- src/lib/ShowCode.svelte
+++ src/lib/ShowCode.svelte
@@ -8,12 +8,18 @@
 
 <!-- {highlighted} -->
 
-<div class="relative">
-	<pre class="language-svelte rounded-xl">
-<p
-			class="rounded-r-0 rounded-bl-0 absolute top-0 left-2 rounded-xl bg-orange-400 p-1 text-black">{name}</p>
-<code
-			style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
-monospace;">{@html highlighted}</code>
-  </pre>
+<div>
+	{#if $$slots.default}
+		<div class="rounded-b-0 rounded-xl border-4 border-b-0 border-solid border-orange-500 p-2">
+			<slot />
+		</div>
+	{/if}
+
+	<div class="relative">
+		<pre class="language-svelte rounded-t-0 !m-0 rounded-xl p-0 !pt-10"><p
+				class="rounded-r-0 rounded-bl-0 rounded-t-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
+				style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+      monospace;">{@html highlighted}</code>
+    </pre>
+	</div>
 </div>
