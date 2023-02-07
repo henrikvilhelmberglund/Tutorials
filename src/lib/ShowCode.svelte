6f60ed8c813c16755c881ffbad5c8c54ddc04eed
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-07 17:53:26 +0100

    fix: :lipstick: make top border round when no slots

--- src/lib/ShowCode.svelte
+++ src/lib/ShowCode.svelte
@@ -16,10 +16,18 @@
 	{/if}
 
 	<div class="relative">
-		<pre class="language-svelte rounded-t-0 !m-0 rounded-xl p-0 !pt-10"><p
-				class="rounded-r-0 rounded-bl-0 rounded-t-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
-				style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+		{#if $$slots.default}
+			<pre class="language-svelte rounded-t-0 !m-0 rounded-xl p-0 !pt-10"><p
+					class="rounded-r-0 rounded-bl-0 rounded-t-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
+					style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
       monospace;">{@html highlighted}</code>
     </pre>
+		{:else}
+			<pre class="language-svelte !m-0 rounded-xl p-0 !pt-10"><p
+					class="rounded-r-0 rounded-bl-0  absolute -top-4 left-0 rounded-xl rounded-br-xl bg-orange-500 p-1 text-black">{name}</p><code
+					style="font-family: 'Maple', Menlo, Monaco, Consolas, 'Andale Mono', 'Ubuntu Mono', 'Courier New',
+        monospace;">{@html highlighted}</code>
+      </pre>
+		{/if}
 	</div>
 </div>
