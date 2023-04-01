174cc858f870b46eae33db991ccc0f413237d521
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-01 20:46:30 +0200

    fix: :wheelchair: WCAG

--- src/lib/components/A.svelte
+++ src/lib/components/A.svelte
@@ -3,6 +3,6 @@
 	export let rel = "what";
 </script>
 
-<a class="text-1xl font-sans font-medium text-pink-600 underline" {href}>
+<a class="text-1xl font-sans font-medium text-pink-700 underline" {href}>
 	<slot />
 </a>
