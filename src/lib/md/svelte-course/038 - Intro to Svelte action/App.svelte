c831340ad2d5c35dacd6ccb51fe4ed90ef49a4fb
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-02 00:53:26 +0200

    feat: :sparkles: add 038 intro to svelte action

---
+++ src/lib/md/svelte-course/038 - Intro to Svelte action/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	let show = true;
+	function action(node, params) {
+    let myTimeout;
+		console.log("hi! I was created!", node);
+		if (params) {
+			myTimeout = setTimeout(() => {
+				node.innerHTML = params;
+				console.log("wow! I was changed!");
+			}, 3000);
+		}
+		return {
+			destroy() {
+				console.log("bye! I was destroyed");
+				if (params) {
+					clearTimeout(myTimeout);
+				}
+			},
+		};
+	}
+</script>
+
+<input type="checkbox" bind:checked={show} />
+
+{#if show}
+	<div use:action>Hello</div>
+	<h1 use:action={"My text changed! 😊"}>My text will change soon!</h1>
+{/if}
+
+<style>
+</style>
