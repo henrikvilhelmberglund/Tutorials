b332b88ebb15a3ad197534502f1f39b071450b08
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-02 16:35:20 +0200

    feat: :sparkles: add 039 dynamic parameters in Svelte actions

---
+++ src/lib/md/svelte-course/039 - Dynamic parameters in Svelte actions/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	let name = "world";
+
+	function lihauAction(node, params) {
+		console.log(node, params);
+		node.innerHTML = params;
+		return {
+			destroy() {
+				console.log("node was destroyed");
+			},
+		};
+	}
+</script>
+
+<input bind:value={name} />
+
+<div use:lihauAction={name} />
+
+name: {name}
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/039 - Dynamic parameters in Svelte actions/App2.svelte
@@ -0,0 +1,26 @@
+<script>
+	let name = "world";
+
+	function lihauAction(node, params) {
+		console.log(node, params);
+		node.innerHTML = params;
+		return {
+			update(newParams) {
+				console.log("updated!", newParams);
+				node.innerHTML = newParams;
+			},
+			destroy() {
+				console.log("node was destroyed");
+			},
+		};
+	}
+</script>
+
+<input bind:value={name} />
+
+<div use:lihauAction={name} />
+
+name: {name}
+
+<style>
+</style>
