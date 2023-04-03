2c54b191872c55eb15cb15c74cedaf12b420dbff
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-03 17:17:36 +0200

    fix: :bug: fix build by adding flatpickr instead of using cdn

--- src/lib/md/svelte-course/041 - Integrating UI libraries with Svelte actions/UsingAction.svelte
+++ src/lib/md/svelte-course/041 - Integrating UI libraries with Svelte actions/UsingAction.svelte
@@ -1,13 +1,9 @@
 <script>
+	import flatpickr from "flatpickr";
 	function datepicker(element) {
 		flatpickr(element);
 	}
 </script>
-
-<svelte:head>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
-	<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-</svelte:head>
 
 <button use:datepicker>Date Picker</button>
 
