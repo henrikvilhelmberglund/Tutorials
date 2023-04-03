ca8f8170a10b2a1ab460811843f35f013a1f9df9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-04 00:29:33 +0200

    fix: :lipstick: put modal in middle

--- src/lib/md/svelte-course/045 - use clickOutside/App2.svelte
+++ src/lib/md/svelte-course/045 - use clickOutside/App2.svelte
@@ -34,7 +34,7 @@
 		transition:blur
 		use:clickOutside
 		class="z-100 border-1
-  fixed top-24 left-[50%] rounded-md border-solid border-black bg-green-400 p-8 text-2xl ">
+  fixed top-24 left-[50%] translate-x-[-50%] rounded-md border-solid border-black bg-green-400 p-8 text-2xl ">
 		Hello, I am a beautiful modal!
 	</div>
 	<div class="fixed top-0 left-0 z-50 h-full w-full backdrop-blur-lg" />
