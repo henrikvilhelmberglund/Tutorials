223ee6fdcf50ef069cbeeb2cb86de3f62fbc5d4f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-03 16:11:07 +0200

    feat: :sparkles: add 044 the order of Svelte actions

---
+++ src/lib/md/svelte-course/044 - The order of Svelte actions/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let valueBeforeAction = "";
+	let valueAfterAction = "";
+
+	function uppercase(element) {
+		function onInput(event) {
+			element.value = element.value.toUpperCase();
+		}
+		element.addEventListener("input", onInput);
+
+		return {
+			destroy() {
+				element.removeEventListener("input", onInput);
+			},
+		};
+	}
+</script>
+
+<input bind:value={valueBeforeAction} use:uppercase />
+
+<input use:uppercase bind:value={valueAfterAction} />
+
+<div>
+	{valueBeforeAction}
+</div>
+
+<div>
+	{valueAfterAction}
+</div>
---
+++ src/lib/md/svelte-course/044 - The order of Svelte actions/App2.svelte
@@ -0,0 +1,30 @@
+<script>
+	let valueBeforeAction = "";
+	let valueAfterAction = "";
+
+	function uppercase(element) {
+		function onInput(event) {
+			element.value = element.value.toUpperCase();
+      event.stopImmediatePropagation()
+		}
+		element.addEventListener("input", onInput);
+
+		return {
+			destroy() {
+				element.removeEventListener("input", onInput);
+			},
+		};
+	}
+</script>
+
+<input bind:value={valueBeforeAction} use:uppercase />
+
+<input use:uppercase bind:value={valueAfterAction} />
+
+<div>
+	{valueBeforeAction}
+</div>
+
+<div>
+	{valueAfterAction}
+</div>
