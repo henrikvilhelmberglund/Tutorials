63f34d863194a7cfafdc46a680ad7385957d5ffc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-06 00:30:37 +0200

    feat: :sparkles: update 046 use tooltip

---
+++ src/lib/md/svelte-course/046 - use tooltip/App4.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { tooltip } from "./tooltip2.js";
+</script>
+
+<h1 title="This is a greeting" use:tooltip>Hello world!</h1>
+
+<!-- adding some room to display the tooltip -->
+<div class="pt-12" />
--- src/lib/md/svelte-course/046 - use tooltip/tooltip.js
+++ src/lib/md/svelte-course/046 - use tooltip/tooltip.js
@@ -1,5 +1,6 @@
 export function tooltip(element) {
-	let div;
+  let div;
+  let title;
 
 	let offset = 5;
 	function mouseOver(event) {
@@ -7,14 +8,17 @@ export function tooltip(element) {
 		div.textContent = element.getAttribute("title");
 		div.classList = "my-tooltip";
 		div.style = `top: ${event.pageY + offset}px; left: ${event.pageX + offset}px;`;
-		document.body.appendChild(div);
+    document.body.appendChild(div);
+    title = element.getAttribute('title');
+		element.removeAttribute('title');
 	}
 	function mouseMove() {
 		div.style.left = `${event.pageX + offset}px`;
 		div.style.top = `${event.pageY + offset}px`;
 	}
 	function mouseLeave() {
-		document.body.removeChild(div);
+    document.body.removeChild(div);
+    element.setAttribute('title', title);
 	}
 
 	element.addEventListener("mouseover", mouseOver);
