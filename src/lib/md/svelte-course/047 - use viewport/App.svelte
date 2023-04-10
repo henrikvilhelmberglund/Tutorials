73071a5371b9825003ec1f45a9b74bb712ac3ef8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-10 20:06:25 +0200

    feat: :sparkles: add 047 use viewport

---
+++ src/lib/md/svelte-course/047 - use viewport/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import viewport from "./useViewportAction";
+</script>
+
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+
+<h1
+	use:viewport
+	on:enterViewport={() => console.log("entered viewport")}
+	on:exitViewport={() => console.log("exited viewport")}>
+	Hello world!
+</h1>
+
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+
+<style>
+</style>
