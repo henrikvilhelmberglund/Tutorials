90aefad47e5fdeafec50a625f987575f1a9ae981
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-20 23:40:55 +0200

    feat: :lipstick: add tailwind reset, improve styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,23 @@
 <script>
 	export let data;
 	import { base } from "$app/paths";
-	// TODO enable reset and fix CSS
-	// import '@unocss/reset/antfu.css'
+	import "@unocss/reset/tailwind.css";
 </script>
 
-<nav>
-	<a class="m-4 rounded-lg bg-green-300  p-4 text-black no-underline" href="{base}/"> Home </a>
-	<a class="rounded-lg bg-green-300 p-4 text-black no-underline" href="{base}/svelte-course">
+<nav class="flex pb-4">
+	<a
+		class="mx-4 rounded-lg bg-green-400  p-4 text-black no-underline hover:bg-green-300"
+		href="{base}/">
+		Home
+	</a>
+	<a
+		class="rounded-lg bg-green-400 p-4 text-black no-underline hover:bg-green-300"
+		href="{base}/svelte-course">
 		Svelte Course
 	</a>
 </nav>
 
-<div class="w-full bg-red-900 [&>*]:p-2">
+<div class="w-full bg-red-900">
 	<p class="text-6xl text-red-300">Warning!</p>
 	<p class="text-3xl text-red-100">under construction :)</p>
 </div>
