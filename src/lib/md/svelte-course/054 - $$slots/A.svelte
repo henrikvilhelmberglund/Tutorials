f0eab0c5a74cb2bccc539b209664f3e586e8f559
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-23 10:52:58 +0200

    feat: :sparkles: add svelte-course 054 $$slots

---
+++ src/lib/md/svelte-course/054 - $$slots/A.svelte
@@ -0,0 +1,14 @@
+<div>
+	A: <slot name="a">Fallback for a</slot>
+</div>
+
+<!-- this means that if nothing was passed into the slot b, don't render it -->
+{#if $$slots.b}
+	<div>
+		B: <slot name="b">Fallback for b</slot>
+	</div>
+{/if}
+
+<div>
+	C: <slot name="c">Fallback for c</slot>
+</div>
---
+++ src/lib/md/svelte-course/054 - $$slots/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import A from "./A.svelte";
+</script>
+
+<A />
+
+<A>
+	<div slot="a">Something for a</div>
+	<div slot="b">Something for b</div>
+	<div slot="c">Something for c</div>
+</A>
+
+<hr class="border-black my-4">
+
+<A>
+	<div slot="a">Something for a</div>
+  <!-- note that this still says B: because passing in an empty slot still counts as something-->
+	<div slot="b"></div>
+	<div slot="c">Something for c</div>
+</A>
+
+<style>
+</style>
