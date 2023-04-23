747e0a7b87da7a812109d2b37f482e7d2a57ae3e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-23 22:25:40 +0200

    feat: :sparkles: add Nav.svelte

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { base } from "$app/paths";
+	export let data;
+	export let subroute = "";
+</script>
+
+<nav class="w-[10%] bg-white">
+	{#if data.myPages}
+		<div class="flex flex-col [&>*]:m-4">
+			{#each data.myPages as link}
+				<br />
+				<a
+					class:!bg-orange-400={link.title === data.slug}
+					class="hover:(outline-4 outline-solid) my-1 rounded-lg bg-slate-200 p-2 outline-black"
+					href="{base}{subroute}/{link.title}">{link.title}</a>
+			{/each}
+		</div>
+	{/if}
+</nav>
+
+<style>
+</style>
