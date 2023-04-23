fd781b4260e6cbd0afcbd99e1cded899cfa93463
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-23 22:27:07 +0200

    fix: :bug: don't load svelte-course stuff on main page load

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,11 +4,11 @@ export const prerender = true;
 
 const modules = import.meta.glob([
 	"$lib/md/*.svx",
-	"$lib/md/svelte-course/*/*.svx",
-	// "$lib/md/svelte-course/*.svx",
 	"!$lib/md/_*.svx",
-	"!$lib/md/svelte-course/_*.svx",
-	"!$lib/md/svelte-course/*/_*.svx",
+	// "$lib/md/svelte-course/*/*.svx",
+	// "$lib/md/svelte-course/*.svx",
+	// "!$lib/md/svelte-course/_*.svx",
+	// "!$lib/md/svelte-course/*/_*.svx",
 ]);
 let myPages = [];
 let svelte;
@@ -17,27 +17,11 @@ for (let path in modules) {
 	// console.log(path);
 	let fixedPath = path.replace(".svx", "");
 	let post;
-	if (path.includes("svelte-course")) {
-		if (!path.includes("+page.svx")) {
-			continue;
-		} else {
-			fixedPath = path.replace("/+page.svx", "");
-			// console.log(
-			// 	`$lib/md/svelte-course/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}/+page.svx`
-			// );
 
-			// TODO I probably don't want to do this in the layout, I only want the titles for the nav
-			post = await import(
-				`$lib/md/svelte-course/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}/+page.svx`
-			);
-			title = fixedPath.substring(fixedPath.indexOf("/", 17) + 1, fixedPath.length);
-			svelte = true;
-		}
-	} else {
-		post = await import(`$lib/md/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}.svx`);
-		svelte = false;
-		title = fixedPath.substring(fixedPath.lastIndexOf("/") + 1);
-	}
+	post = await import(`$lib/md/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}.svx`);
+	svelte = false;
+	title = fixedPath.substring(fixedPath.lastIndexOf("/") + 1);
+
 	// let fixedPath = path;
 	myPages.push({
 		title: title,
