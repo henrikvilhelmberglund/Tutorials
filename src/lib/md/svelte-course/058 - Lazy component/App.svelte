5d3aeb6b426de7e66fa8997fcbca0f20b5b6f597
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-25 22:48:10 +0200

    feat: :sparkles: start svelte-course 058 but time to sleep!

---
+++ src/lib/md/svelte-course/058 - Lazy component/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	// When using import here, the component will be loaded at the same time as App.svelte which is not what we want.
+	// import Component from "./Component.svelte";
+
+	// This will return a promise that will be resolved when this file is fetched and ready to use
+	const componentPromise = import("./Component.svelte");
+</script>
+
+{#await componentPromise}
+	Loading...
+{:then { default: Component }}
+	<Component foo="123" bar={456} />
+{:catch error}
+	{error}
+{/await}
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/058 - Lazy component/Component.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let foo;
+  export let bar;
+</script>
+
+Foo: {foo}
+Bar:  {bar}
+
+<style>
+  
+</style>
