c82f40e64f80b3cb65e6b33304b3d46990d34154
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-25 22:36:18 +0200

    fix: :zap: get titles directly from load function

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <nav class="w-[10%] bg-white">
-	{#if data.myPages}
+	{#if data.titles}
 		<div class="flex flex-col [&>*]:m-4">
-			{#each data.myPages as link}
+			{#each data.titles as link}
 				<br />
 				<a
-					class:!bg-orange-400={link.title === data.slug}
+					class:!bg-orange-400={link === data.slug}
 					class="hover:(outline-4 outline-solid) my-1 rounded-lg bg-slate-200 p-2 outline-black"
-					href="{base}{subroute}/{link.title}">{link.title}</a>
+					href="{base}{subroute}/{link}">{link}</a>
 			{/each}
 		</div>
 	{/if}
