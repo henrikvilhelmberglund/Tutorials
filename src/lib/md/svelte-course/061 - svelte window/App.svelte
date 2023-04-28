706e57521b2ffbd21ede7102fce4b093f43142b2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-28 17:15:13 +0200

    feat: :sparkles: add svelte-course 061 svelte window

---
+++ src/lib/md/svelte-course/061 - svelte window/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { browser } from "$app/environment";
+	let innerWidth = 0;
+	import { onMount } from "svelte";
+
+	let searchBar;
+
+	onMount(() => {
+		innerWidth = window.innerWidth;
+		function onResize() {
+			innerWidth = window.innerWidth;
+		}
+		window.addEventListener("resize", onResize);
+		return () => window.removeEventListener("resize", onResize);
+	});
+
+	onMount(() => {
+		function onKeydown(event) {
+			if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === "f") {
+				searchBar.focus();
+				event.preventDefault();
+			}
+		}
+		window.addEventListener("keydown", onKeydown);
+		return () => window.removeEventListener("keydown", onKeydown);
+	});
+</script>
+
+<input bind:this={searchBar} placeholder="Search for..." />
+
+<div>
+	Width: {innerWidth}
+</div>
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/061 - svelte window/App2.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { browser } from "$app/environment";
+	let innerWidth = 0;
+	import { onMount } from "svelte";
+
+	// for SSR
+	if (browser) {
+		innerWidth = window.innerWidth;
+	}
+
+	let searchBar;
+
+	function onKeydown(event) {
+		if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === "f") {
+			searchBar.focus();
+			event.preventDefault();
+		}
+	}
+</script>
+
+<svelte:window on:keydown={onKeydown} bind:innerWidth />
+
+<input bind:this={searchBar} placeholder="Search for..." />
+
+<div>
+	Width: {innerWidth}
+</div>
+
+<style>
+</style>
