1aaf7b9ac6df8bbf1311336e45b607b8ae043cbc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-28 17:14:52 +0200

    feat: :sparkles: add svelte-course 060 svelte self

---
+++ src/lib/md/svelte-course/060 - svelte self/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let data = [1, 2, 3, 4, 5];
+</script>
+
+<span class="m-1 inline-block border border-solid border-blue-500">
+	{#each data as item}
+		<span class="m-1 inline-block border border-solid border-red-500">{item}</span>
+	{/each}
+</span>
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/060 - svelte self/App2.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let data = [1, [2, [3], 4], 5];
+</script>
+
+<span class="m-1 inline-block border border-solid border-blue-500">
+	{#each data as item}
+		<span class="m-1 inline-block border border-solid border-red-500">
+			{#if Array.isArray(item)}
+				<svelte:self data={item} />
+			{:else}
+				{item}
+			{/if}
+		</span>
+	{/each}
+</span>
+
+<style>
+</style>
