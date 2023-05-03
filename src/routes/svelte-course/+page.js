27375380b3305fdfb7a72a37e70d4d9a6b699c0f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-03 18:17:40 +0200

    feat: :sparkles: add box page for svelte-course

--- src/routes/svelte-course/+page.js
+++ src/routes/svelte-course/+page.js
@@ -1,66 +1,48 @@
+// TODO clean up this mess
 // export const modules = import.meta.glob("./*.svx");
 export const prerender = true;
 // export const ssr = false;
 
+let modules = import.meta.glob([
+	// "$lib/md/*.svx",
+	// "!$lib/md/_*.svx",
+	"../../lib/md/svelte-course/*/+page.svx",
+	"!../../lib/md/svelte-course/_*.svx",
+	"!../../lib/md/svelte-course/*/_*.svx",
+	"!../../lib/md/svelte-course/_*/*.svx",
+]);
 let myPages = [];
-let titles = [];
 let svelte;
 let title;
+let titles = [];
 let posts = [];
+for (let path in modules) {
+	// console.log(path);
+	let fixedPath = path.replace(".svx", "");
+	let post;
 
-async function importPosts() {
-	const modules = import.meta.glob([
-		"../../lib/md/svelte-course/*/*.svx",
-		"!../../lib/md/svelte-course/_*.svx",
-		"!../../lib/md/svelte-course/*/_*.svx",
-		"!../../lib/md/svelte-course/_*/*.svx",
-	]);
+	// post = import(`../../../lib/md/svelte-course/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}.svx`);
+	// posts.push(post);
+	svelte = false;
+	// title = fixedPath.substring(fixedPath.lastIndexOf("/") + 1);
+  fixedPath = path.replace("/+page.svx", "");
+  title = fixedPath.substring(fixedPath.indexOf("/", 17) + 1, fixedPath.length);
 
-	console.log(modules);
-	for (let path in modules) {
-		console.log(path);
-		let fixedPath = path.replace(".svx", "");
-		let post;
-		if (!path.includes("+page.svx")) {
-			continue;
-		} else {
-			fixedPath = path.replace("/+page.svx", "");
-			console.log(fixedPath);
-			// console.log(
-			// 	`$lib/md/svelte-course/${fixedPath.substring(fixedPath.lastIndexOf("/") + 1)}/+page.svx`
-			// );
-
-			// TODO I probably don't want to do this in the layout, I only want the titles for the nav
-			post = import(
-				`../../lib/md/svelte-course/${fixedPath.substring(
-					fixedPath.lastIndexOf("/") + 1
-				)}/+page.svx`
-			);
-			posts.push(post);
-			title = fixedPath.substring(fixedPath.indexOf("/", 17) + 1, fixedPath.length);
-			svelte = true;
-		}
-
-		// let fixedPath = path;
-		myPages.push({
-			title: title,
-			link: fixedPath,
-			original: path,
-			newbody: post.default,
-			bsvelte: svelte,
-		});
-		titles.push(title);
-		console.log(posts);
-	}
-	return myPages;
+	// let fixedPath = path;
+	myPages.push({
+		title: title,
+		link: fixedPath,
+		original: path,
+		// newbody: post.default,
+		bsvelte: svelte,
+	});
+	titles.push(title);
 }
 
 export async function load({ params }) {
 	return {
-    // TODO this naming is a bit stupid
-		myPages: importPosts(),
-		posts: posts,
-		// myPages:
+		myPages,
+		// posts: posts,
 		titles: titles,
 		title: "Tutorials",
 		// meta: post.metadata
---
+++ src/routes/svelte-course/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	import PostGrid from "$lib/PostGrid.svelte";
+
+	export let data;
+</script>
+
+<div class="flex">
+	<Nav {data} subroute={"/svelte-course"} />
+	<PostGrid route="/svelte-course" posts={data.titles} />
+</div>
+
+<style>
+</style>
