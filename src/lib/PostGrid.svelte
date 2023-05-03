780aee981a1e2b2949e1262b909353198048ab5c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-03 18:41:44 +0200

    fix: :lipstick: make boxes bigger

--- src/lib/PostGrid.svelte
+++ src/lib/PostGrid.svelte
@@ -6,11 +6,11 @@
 	posts = ["all", ...posts];
 </script>
 
-<div class="flex flex-wrap items-center justify-center gap-24 pt-24">
+<div class="flex flex-wrap items-center h-min justify-center gap-6 pt-10">
 	{#each posts as post}
 		{@const link = post === "all" ? "View all" : post.includes("-") ? post.split("-")[1] : post}
 		<a
-			class="hover:(outline-black outline-solid outline-2) flex h-64 w-64 flex-col justify-center rounded-lg  bg-white p-4 text-center text-3xl shadow-xl"
+			class="hover:(outline-black outline-solid outline-2) w-48 h-48 md:w-96 md:h-96 flex flex-col justify-center rounded-lg  bg-white p-4 text-center text-3xl shadow-xl"
 			href="{base}{route}/{post}">{link}</a>
 	{/each}
 </div>
