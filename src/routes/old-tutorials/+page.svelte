a065a80245335c156cbae62b3427d78396aca608
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-03 17:48:34 +0200

    feat: :sparkles: display links as boxes in the main page

---
+++ src/routes/old-tutorials/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	import PostGrid from "$lib/PostGrid.svelte";
+
+	export let data;
+</script>
+<div class="flex">
+  
+  <Nav {data} subroute={"/old-tutorials"} />
+  <PostGrid route="/old-tutorials" posts={data.titles} />
+</div>
+
+<style>
+</style>
