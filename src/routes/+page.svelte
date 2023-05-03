7be610008b5d9f053a7d049cae1ed388a6cc1192
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-04 00:20:45 +0200

    feat: :lipstick: update index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,23 @@
 	];
 </script>
 
-<div class="flex">
-	{#each routes as route, i}
-		<div>
-			<a
-				class="mx-4 rounded-lg bg-green-400  p-4 text-black no-underline hover:bg-green-300"
-				href="{base}{route.link}">
-				{route.name}
-			</a>
-		</div>
-	{/each}
+<div class="flex flex-col items-center">
+	<div>
+		<h1 class="text-3xl">Welcome to my tutorials page!</h1>
+	</div>
+
+	<div class="flex w-full items-center justify-evenly">
+		{#each routes as route, i}
+			<div class="flex h-96 w-full">
+				<!-- TODO add images for these instead of text -->
+				<a
+					class="mx-4 flex w-full flex-col items-center justify-center rounded-lg bg-white p-4 text-4xl text-black no-underline hover:bg-green-300"
+					href="{base}{route.link}">
+					{route.name}
+				</a>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
