b2036d4c840cd9d6fc263803cda9957c20b08115
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-03 17:48:05 +0200

    feat: :sparkles: add PostGrid component

---
+++ src/lib/PostGrid.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { base } from "$app/paths";
+	export let posts;
+	export let route;
+</script>
+
+<div class="flex flex-wrap items-center justify-center gap-24 pt-24">
+	<a
+		class="hover:(outline-black outline-solid outline-2) flex  h-64 w-64 flex-col justify-center rounded-lg bg-white p-4 text-center text-3xl shadow-xl"
+		href="{base}{route}/all">
+		View all
+	</a>
+	{#each posts as post}
+		{@const link = post.includes("-") ? post.split("-")[1] : post}
+		<a
+			class="hover:(outline-black outline-solid outline-2) flex h-64 w-64 flex-col justify-center rounded-lg  bg-white p-4 text-center text-3xl shadow-xl"
+			href="{base}{route}/{post}">{link}</a>
+	{/each}
+</div>
+
+<style>
+</style>
