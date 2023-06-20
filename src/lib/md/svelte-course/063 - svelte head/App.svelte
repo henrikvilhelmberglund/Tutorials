cf91d311b1a44ce43a60b31a6f57d5a76e2056e1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-20 21:00:25 +0200

    feat: :sparkles: add svelte course 063 - svelte head

---
+++ src/lib/md/svelte-course/063 - svelte head/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	let name = "world";
+</script>
+
+<input bind:value={name} />
+<svelte:head>
+	<title>{name}</title>
+	<meta name="description" content={name} />
+</svelte:head>
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/063 - svelte head/App2.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = "";
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Kablammo&display=swap" rel="stylesheet" />
+	{@html `<style>:root { --font-color: ${name}}</style>`}
+</svelte:head>
+
+<input bind:value={name} placeholder="Try typing a color like red!" />
+<p class="fancy-text">This is some fancy text!</p>
+
+<style>
+	.fancy-text {
+		font-family: "Kablammo", cursive;
+		font-size: 3rem;
+		color: var(--font-color);
+	}
+</style>
