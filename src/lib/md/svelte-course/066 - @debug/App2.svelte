92d3754b408b30b1476ac7e4df3942f061bdc675
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-21 17:52:37 +0200

    feat: :sparkles: add svelte-course 066 @debug

---
+++ src/lib/md/svelte-course/066 - @debug/App2.svelte
@@ -0,0 +1,16 @@
+<script>
+	let array = [{ value: 1 }, { value: 2 }, { value: 3 }];
+	let value = "";
+</script>
+
+<input bind:value />
+{@debug value}
+
+{#each array as item, i}
+	<li>
+		<input type="number" bind:value={item.value} />
+	</li>
+{/each}
+
+<style>
+</style>
---
+++ src/lib/md/svelte-course/066 - @debug/App3.svelte
@@ -0,0 +1,16 @@
+<script>
+	let array = [{ value: 1 }, { value: 2 }, { value: 3 }];
+	let value = "";
+</script>
+
+<input bind:value />
+
+{#each array as item, i}
+	{@debug item}
+	<li>
+		<input type="number" bind:value={item.value} />
+	</li>
+{/each}
+
+<style>
+</style>
