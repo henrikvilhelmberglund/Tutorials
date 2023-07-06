7e41497188dcf9f6b3b11df24409d07c4b7ea91b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-06 22:47:53 +0200

    feat: :sparkles: add hover info file for 074 App2

---
+++ src/lib/md/svelte-course/074 - Flipboard transition/App2Hover.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export const hover = {
+		20: "By throttling the tick function we can make the transition run slower even though it is called every tick by Svelte.",
+		27: "Here is the main logic. We start by making the string as long as the real word and filling it with dashes. After some time we replace the dashes with random characters, then finally with the real character.",
+	};
+</script>
+
+<script>
+</script>
+
+<style>
+</style>
