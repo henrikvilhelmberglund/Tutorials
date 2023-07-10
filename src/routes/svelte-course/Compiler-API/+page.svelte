c6baaf96a6769cdabb5732eb469f36fb986579ed
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-10 17:01:08 +0200

    feat: :sparkles: add svelte-course Svelte compiler API

---
+++ src/routes/svelte-course/Compiler-API/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import ShowMany from "$lib/ShowMany.svelte";
+	import CompileComponent from "./CompileComponent.svelte";
+	import CompileComponentRaw from "./CompileComponent.svelte?raw";
+</script>
+
+<ShowMany
+	name={[
+		{
+			name: "CompileComponent",
+			comp: CompileComponent,
+			raw: CompileComponentRaw,
+			text: "Our code looks something like this.",
+		},
+	]} />
+
+<style>
+</style>
---
+++ src/routes/svelte-course/Compiler-API/CompileComponent.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { compile } from "svelte/compiler";
+	import fs from "fs";
+	import path from "path";
+	const pathToComponent = path.join(
+		"src/routes/svelte-course/Compiler-API/Component.svelte"
+	);
+	const svelteCode = fs.readFileSync(pathToComponent, "utf-8");
+	const { js, css } = compile(svelteCode, {
+		// options here
+		filename: "CompiledComponent",
+		// format: "cjs", // or esm
+		// generate: "ssr" // or dom
+		// dev: true
+		// immutable: true
+		// legacy: true
+		// sveltePath:  "",
+		// css: false,
+		// cssHash: ({ hash, css, name, filename }) => `xyz${hash(css)}`,
+		// and more! (see docs at https://svelte.dev/docs/svelte-compiler)
+	});
+	console.log(js, css);
+</script>
+
+<p>Result:</p>
+
+{JSON.stringify(js)}
+
+{JSON.stringify(css)}
+
+<style>
+</style>
