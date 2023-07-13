a9452bc8e0b9775d895ad34c67f63e1db8ecfb13
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-13 16:53:46 +0200

    fix: :speech_balloon: make it more apparent that View all is laggy

--- src/lib/PostGrid.svelte
+++ src/lib/PostGrid.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex flex-wrap items-center h-min justify-center gap-6 pt-10">
 	{#each posts as post}
-		{@const link = post === "all" ? "View all" : post.includes("-") ? post.split("-")[1] : post}
+		{@const link = post === "all" ? "View all (laggy 😎)" : post.includes("-") ? post.split("-")[1] : post}
 		<a
 			class="hover:(outline-black outline-solid outline-1) w-48 h-48 md:w-96 md:h-96 flex flex-col justify-center rounded-lg  bg-white p-4 text-center text-3xl shadow-xl"
 			href="{base}{route}/{post}">{link}</a>
