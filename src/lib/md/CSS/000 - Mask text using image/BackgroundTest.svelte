c29f0acc49dfaf691d6ec2335d84df4ad73f2931
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 23:45:37 +0200

    feat(md: CSS): :sparkles: add mask text using image

---
+++ src/lib/md/CSS/000 - Mask text using image/BackgroundTest.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<p>🦆</p>
+
+<style>
+	@keyframes test {
+		from {
+			background-position: -4rem -10rem;
+		}
+		to {
+			background-position: -4rem 7rem;
+		}
+	}
+	p {
+		background-image: url("851px-Svelte_Logo.svg.png");
+		background-size: 10%;
+		background-clip: text;
+		background-position: -4rem 0rem;
+		-webkit-background-clip: text;
+		color: transparent;
+		animation-name: test;
+		animation-duration: 1s;
+		animation-iteration-count: infinite;
+		animation-direction: alternate;
+		/* -webkit-text-fill-color: aqua; */
+		/* -webkit-text-fill-color: transparent; */
+		font-size: 11rem;
+	}
+</style>
---
+++ src/lib/md/CSS/000 - Mask text using image/Cat.svelte
@@ -0,0 +1,23 @@
+<script>
+	let text = Array.from({ length: 3000 }).map((a) => {
+		return "@";
+	});
+</script>
+
+<div
+	class="flex max-w-[600px] break-all bg-[url('manja-vitolic-gKXKBY-C-Dk-unsplash.jpg')] bg-clip-text text-[0.7rem] leading-3 tracking-[-5px] text-transparent">
+	<span class="m-0 p-0">{text.join("")}</span>
+</div>
+
+<style>
+	/* Vanilla CSS: */
+	/* 
+  div {
+	background-image: url("manja-vitolic-gKXKBY-C-Dk-unsplash.jpg");
+	background-clip: text;
+	-webkit-background-clip: text;
+	color: transparent;
+	font-size: 0.7rem;
+	} 
+  */
+</style>
