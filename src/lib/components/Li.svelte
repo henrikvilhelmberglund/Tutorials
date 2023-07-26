634413d4f2f2989ba7245e0070f26ecad3e3634f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-26 15:51:25 +0200

    feat: :lipstick: edit styling

--- src/lib/components/Li.svelte
+++ src/lib/components/Li.svelte
@@ -1,3 +1,3 @@
-<li class="text-1xl list-inside list-disc font-sans font-medium text-pink-700">
+<li class="text-1xl list-inside list-none font-sans font-medium text-pink-700">
 	<slot />
 </li>
--- src/lib/components/Ul.svelte
+++ src/lib/components/Ul.svelte
@@ -1,3 +1,3 @@
-<ul class="ml-6 !p-4 self-start rounded-3xl bg-slate-200 shadow-xl">
+<ul class="my-4 ml-6 self-start rounded-xl bg-slate-200 !p-4 shadow-xl">
 	<slot />
 </ul>
